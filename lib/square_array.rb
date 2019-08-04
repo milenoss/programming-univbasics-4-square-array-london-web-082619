d2fa29f40dfd5cf4f35f62ca67dcdb77e5f3608b
Milen Khanal <milenkhanal@gmail.com>
2019-08-04 21:26:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,3 +8,13 @@ def square_array(array)
   end
 return newarray
 end
+
+def square_array(array)
+  newarray = []
+  counter = 0 
+  while (counter < array.length) do
+    newarray.push(array[counter]**2)
+    counter +=1 
+  end
+  return newarray
+end
